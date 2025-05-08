b1ce64fbd10ee033c41f91b17bb520e50b8e383c
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-05-08 23:03:09 +0100

    Add FilePower1

---
+++ FilePower1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string file = args[0];
+            Queue<string> queueLines = new Queue<string>();
+
+            while (true)
+            {
+                string newLine = Console.ReadLine();
+                if (string.IsNullOrEmpty(newLine)) {break;}
+                else {queueLines.Enqueue(newLine);}
+            }
+
+            File.WriteAllLines(file, queueLines);
+        }
+    }
+}
