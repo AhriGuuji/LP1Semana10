1d93d13fb657aa11174cd0cce627e4556a57f27b
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-05-09 14:30:28 +0100

    Add CharSets

---
+++ CharSets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace CharSets
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string thing;
+            char[] c;
+            HashSet<char> chars = new HashSet<char>();
+
+            foreach(string arg in args)
+            {
+                using StreamReader readIt = new StreamReader(arg);
+
+                while( ( thing = readIt.ReadLine()) != null)
+                {
+                    c = thing.ToCharArray();
+                    if (c.Length > 1) { break; }
+                    chars.Add(c[0]);
+                }
+            }
+
+            List<char> chars1 = new List<char>(chars);
+            chars1.Sort();
+
+            foreach(char letter in chars1)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
